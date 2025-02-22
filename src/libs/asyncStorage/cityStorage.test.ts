6859b0eddce1e9800f2065d6d5e4d81e2b8f160b
Lucas Farias <lucas.farias@rtbhouse.com>
2025-02-22 16:30:29 -0300

    feat: finish async storage tests

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,38 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage";
+import {
+  getStorageCity,
+  removeStorageCity,
+  saveStorageCity,
+} from "@libs/asyncStorage/cityStorage";
+import { CityProps } from "@services/getCityByNameService";
+
+const newCity: CityProps = {
+  id: "2",
+  name: "São Paulo",
+  latitude: 123,
+  longitude: 456,
+};
 
 describe("Storage: CityStorage", () => {
   it("should return null when no city is stored", async () => {
-    const response = await getStorageCity()
-    
-    expect(response).toBeNull()
-  })
+    const response = await getStorageCity();
+
+    expect(response).toBeNull();
+  });
+
+  it("should return the stored city", async () => {
+    await saveStorageCity(newCity);
+
+    const response = await getStorageCity();
+
+    expect(response).toStrictEqual(newCity);
+  });
+
+  it("should remove the stored city", async () => {
+    await saveStorageCity(newCity);
+    await removeStorageCity();
+
+    const response = await getStorageCity();
+
+    expect(response).toBeNull();
+  });
 });
